eaf3c05cceb388f1aff69e6af37ddc7163ca25fc
karenmarzuniga-spec <karenmarzuniga@gmail.com>
2026-08-20 11:23:00 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -1,5 +1,19 @@
--- Adding comments
--- Basic SQL 
+-- Basic SQL
+---- CREATE table is a statement that creates a new table in a satabase
+---- DROP table removes a table in database
+---- SELECT allows you to read data and display it. Called querey
+---- SELECT specifies which columns you want o be given data for
+---- FROM specifies which tabel you want to sleect columns from
+---- astricks means all
+
+SELECT id, account_id
+-- OR
+  SELECT *
+FROM orders;
+
+
+
+
 -- SQL Joins
 -- Aggregations
 -- SQL Subqueries * Temporary Tables
